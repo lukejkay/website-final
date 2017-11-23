0a745170b13ab0ae767f08538acd9048ee2b71bb
Luke Archer <P14177187@LEC-ADMIN.dmu.ac.uk>
2017-11-23 12:42:39 +0000

    Began the javascript for contact form, other various changes to styling

---
+++ contactform.js
@@ -0,0 +1,8 @@
+let submit1 = document.getElementById("submit");
+let name = document.getElementById("namefield");
+let email = document.getElementById("emailfield");
+let txtarea = document.getElementById("textareafield");
+
+name.onkeypress = makebuttonclick();
+
+	submit1.setAttribute("style", "pointer-events:auto;");

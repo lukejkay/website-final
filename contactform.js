e73207c36961ca1ab81e62fc173ba146228b6291
Luke Archer <P14177187@LEC-ADMIN.dmu.ac.uk>
2017-11-30 13:54:09 +0000

    complete js for contact form validation and autofill email

--- contactform.js
+++ contactform.js
@@ -2,7 +2,7 @@ let submit1 = document.getElementById("submit");
 let name = document.getElementById("namefield");
 let email = document.getElementById("emailfield");
 let txtarea = document.getElementById("textareafield");
-
+let emailcomplete = document.getElementById("email");
 
 function checkEmailDomain(e){
     let checkvalue;
@@ -10,14 +10,46 @@ function checkEmailDomain(e){
     if(window.event) {                  
       checkvalue = e.keyCode;
 	  console.log(checkvalue);
-	  if (checkvalue == 64) {
-		  
-		  alert(String.fromCharCode(checkvalue)); // need to remember it is numbers not chars
-	  }
+	  
     }
-
-    
-  }
+	let email1 = email.value + String.fromCharCode(checkvalue);
+    console.log("full email = " + email1);
+	if (email.value.indexOf("@") > -1){
+		let splitEmail = email1.split("@")[1];
+		console.log("email split = " + email1.split("@")[1]);
+		firstcharafterat = splitEmail.charAt(0);
+		console.log("splitmail = " + firstcharafterat);
+		
+		if(firstcharafterat == "g") {
+			 email.value = email.value + "googlemail.com";
+			 return false;
+			}
+		else if(firstcharafterat == "h") {
+			 email.value = email.value + "hotmail.com";
+			 return false;
+			}
+		else if(firstcharafterat == "o") {
+			 email.value = email.value + "outlook.com";
+			 return false;
+			}
+		else if(firstcharafterat == "y") {
+			 email.value = email.value + "yahoo.com";
+			 return false;
+			}
+		else if(firstcharafterat == "l") {
+			 email.value = email.value + "live.com";
+			 return false;
+			}
+		else if(firstcharafterat == "a") {
+			 email.value = email.value + "aol.com";
+			 return false;
+			}
+		else if(firstcharafterat == "n") {
+			 email.value = email.value + "ntlworld.com";
+			 return false;
+			}
+	}
+}
 
 function formcheck() {
 	console.log(name.value);

7a43b55263d3a4a4260f61e5b8a14ed43b90fea0
Luke Archer <P14177187@LEC-ADMIN.dmu.ac.uk>
2017-11-30 12:35:19 +0000

    Validation js for contact form, currently working on my email auto complete part

--- contactform.js
+++ contactform.js
@@ -2,3 +2,60 @@ let submit1 = document.getElementById("submit");
 let name = document.getElementById("namefield");
 let email = document.getElementById("emailfield");
 let txtarea = document.getElementById("textareafield");
+
+
+function checkEmailDomain(e){
+    let checkvalue;
+
+    if(window.event) {                  
+      checkvalue = e.keyCode;
+	  console.log(checkvalue);
+	  if (checkvalue == 64) {
+		  
+		  alert(String.fromCharCode(checkvalue)); // need to remember it is numbers not chars
+	  }
+    }
+
+    
+  }
+
+function formcheck() {
+	console.log(name.value);
+	console.log(email.value);
+	console.log(txtarea.value);
+	
+	
+	if(name.value == "") {
+		alert("Please fill in the name field");
+		return false;
+	}
+	else if (email.value == "") {
+		alert("Please fill in the email field");
+		return false;
+	}
+	else if (validateemail()) {
+		alert("Please add an actual email address");
+		return false;
+	}
+	else if (txtarea.value == "") {
+		alert("Please fill in the TextBox field");
+		return false;
+	}
+	else {
+		return true;
+	}
+	
+	//need to return true to show valid
+}
+
+function validateemail() {
+	
+	if (email.value.indexOf("@") > -1) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	
+	
+}

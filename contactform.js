773e36bad49d1ac78d03818935e6c751d4cdc89c
Luke Archer <P14177187@LEC-ADMIN.dmu.ac.uk>
2017-11-30 12:00:15 +0000

    updated icons and complete portfolio

--- contactform.js
+++ contactform.js
@@ -2,7 +2,3 @@ let submit1 = document.getElementById("submit");
 let name = document.getElementById("namefield");
 let email = document.getElementById("emailfield");
 let txtarea = document.getElementById("textareafield");
-
-name.onkeypress = makebuttonclick();
-
-	submit1.setAttribute("style", "pointer-events:auto;");
